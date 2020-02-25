a3a16c8df25cefc7aade3f2aeb39604ef6319bcb
Marco Argentieri <marco@vulpem.com>
2020-02-26 00:03:24 +0100

    Add Liquid related fields for tx size

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # coinselect
 
-[![TRAVIS](https://secure.travis-ci.org/bitcoinjs/coinselect.png)](http://travis-ci.org/bitcoinjs/coinselect)
-[![NPM](http://img.shields.io/npm/v/coinselect.svg)](https://www.npmjs.org/package/coinselect)
-
-[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
-
-An unspent transaction output (UTXO) selection module for bitcoin.
+An unspent transaction output (UTXO) selection module for Liquid-Bitcoin.
 
 **WARNING:** Value units are in `satoshi`s, **not** Bitcoin.
 
--- utils.js
+++ utils.js
@@ -1,9 +1,23 @@
 // baseline estimates, used to improve performance
-var TX_EMPTY_SIZE = 4 + 1 + 1 + 4
-var TX_INPUT_BASE = 32 + 4 + 1 + 4
-var TX_INPUT_PUBKEYHASH = 107
-var TX_OUTPUT_BASE = 8 + 1
-var TX_OUTPUT_PUBKEYHASH = 25
+var TX_VERSION = 4
+var TX_LOCKTIME = 4
+var TX_IN_COUNT = 1
+var TX_OUT_COUNT = 1
+var TX_FLAG = 1
+var TX_EMPTY_SIZE = TX_VERSION + TX_LOCKTIME + TX_IN_COUNT + TX_OUT_COUNT + TX_FLAG
+
+var TX_PREV_OUT_HASH = 32
+var TX_PREV_OUT_INDEX = 4
+var TX_INPUT_SEQUENCE = 4
+var TX_INPUT_SCRIPT_LENGHT = 1  
+var TX_INPUT_BASE = TX_PREV_OUT_HASH + TX_PREV_OUT_INDEX + TX_INPUT_SCRIPT_LENGHT + TX_INPUT_SEQUENCE
+var TX_INPUT_PUBKEYHASH = 91
+
+var TX_OUTPUT_ASSET = 32 + 1
+var TX_OUTPUT_NONCE = 1
+var TX_OUTPUT_VALUE = 8 + 1
+var TX_OUTPUT_BASE = TX_OUTPUT_VALUE + TX_OUTPUT_ASSET + TX_OUTPUT_NONCE 
+var TX_OUTPUT_PUBKEYHASH = 24
 
 function inputBytes (input) {
   return TX_INPUT_BASE + (input.script ? input.script.length : TX_INPUT_PUBKEYHASH)
@@ -44,7 +58,8 @@ var BLANK_OUTPUT = outputBytes({})
 
 function finalize (inputs, outputs, feeRate) {
   var bytesAccum = transactionBytes(inputs, outputs)
-  var feeAfterExtraOutput = feeRate * (bytesAccum + BLANK_OUTPUT)
+  //Here we add an output for the change and the Liquid explicit fee output
+  var feeAfterExtraOutput = feeRate * (bytesAccum + BLANK_OUTPUT + BLANK_OUTPUT)
   var remainderAfterExtraOutput = sumOrNaN(inputs) - (sumOrNaN(outputs) + feeAfterExtraOutput)
 
   // is it worth a change output?
